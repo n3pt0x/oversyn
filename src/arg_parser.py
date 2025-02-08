73cb2f6aa719ef089bb5f354c4fcc9cb5e0ed52b
n3pt0x <157751271+n3pt0x@users.noreply.github.com>
2025-02-08 23:52:23 +0100

    Init parser to get information

---
+++ src/arg_parser.py
@@ -0,0 +1,45 @@
+from src.config import Config
+import argparse
+
+
+class ArgParser():
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(
+            prog="0verSyn",
+            description="Tool for attack DoS.",
+            epilog="Used the --help or -h flag for more information",
+            usage="python main.py <target>"
+        )
+
+        self.parser.add_argument(
+            "target",
+            type=str,
+            help="The IP address of the target"
+        )
+
+        self.parser.add_argument(
+            "-p",
+            "--port",
+            type=int,
+            nargs="+",
+            help="Port of the target number"
+        )
+
+        self.parser.add_argument(
+            "-c",
+            "--count",
+            type=int,
+            default=None,
+            help="Number of requests, default set on infinite"
+        )
+
+        self.parser.add_argument(
+            "-v",
+            "--verbose",
+            action="store_true",
+            help="Used the verbose mode"
+        )
+
+    def parse_args(self):
+        """return args list"""
+        return self.parser.parse_args()
