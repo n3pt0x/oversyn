a999d3a74b3bcee09863df1a987271f2b28ff2fc
n3pt0x <157751271+n3pt0x@users.noreply.github.com>
2025-02-08 23:58:30 +0100

    Create a first structure in main

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from src import show_banner
-
+from src.arg_parser import ArgParser
+from src import TargetValidator
+from src.config import Config
 
 def main():
-    return
+    trying_connection()
 
 
-if __name__ == '__main__':
+def trying_connection():
+    parser = ArgParser()
+    args = parser.parse_args()
+    Config().init(args)
+
+    target_validator = TargetValidator(args.target)
+    target_validated = target_validator.validate()
+    
+    if target_validated:
+        print('Continue')
+    else:
+        print('End !')
+
+
+if __name__ == "__main__":
     show_banner()
+    main()
